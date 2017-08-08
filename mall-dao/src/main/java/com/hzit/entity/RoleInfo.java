4a65d3a378082842f19522e76f589c3c616cc865
Tracy McGrady <895121113@qq.com>
2017-08-08 10:45:05 +0800

    实体类

---
+++ mall-dao/src/main/java/com/hzit/entity/RoleInfo.java
@@ -0,0 +1,34 @@
+package com.hzit.entity;
+
+/**
+ * 　　  　  　      \\\|///
+ * 　　　 　  　  \\　.-.-　//
+ * 　　　　　  　  (　.@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 魏轶伦　　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                   |
+ * |　@create 2017年08月08日 - 10:28
+ * |  @description
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class RoleInfo
+{
+    private int roleId;
+    private String roleName;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
