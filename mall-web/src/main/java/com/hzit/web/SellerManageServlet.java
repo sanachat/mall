c81d6df20172a02f0d75ce5fd6d9c6921bd85479
Tracy McGrady <895121113@qq.com>
2017-08-10 19:23:37 +0800

    jsp

--- mall-web/src/main/java/com/hzit/web/SellerManageServlet.java
+++ mall-web/src/main/java/com/hzit/web/SellerManageServlet.java
@@ -1,8 +1,8 @@
 package com.hzit.web;
 
+import com.hzit.dao.GameGoodInfoDao;
 import com.hzit.dao.SqlSessionHelper;
-import com.hzit.dao.UserInfoDao;
-import com.hzit.entity.UserInfo;
+import com.hzit.entity.GamegoodInfo;
 import org.apache.ibatis.session.SqlSession;
 
 import javax.servlet.ServletException;
@@ -11,23 +11,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Tracy McGrady on 2017/8/9.
+ * Created by Tracy McGrady on 2017/8/10.
  */
-@WebServlet(name = "SellerServlet",value = "/seller")
-public class SellerServlet extends HttpServlet {
+@WebServlet(name = "SellerManageServlet",value = "/sellerManage")
+public class SellerManageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         SqlSession session = SqlSessionHelper.getSqlSession();
-        UserInfoDao dao = session.getMapper(UserInfoDao.class);
-        UserInfo userInfo =(UserInfo)request.getSession().getAttribute("");
-        dao.updateSeller(userInfo.getUserId());
-        session.commit();
-        response.sendRedirect("BecomeSeller.jsp");
+        GameGoodInfoDao dao = session.getMapper(GameGoodInfoDao.class);
+        List<GamegoodInfo> list = dao.findAllGood();
+        request.getSession().setAttribute("list", list);
+        response.sendRedirect("index.jsp");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
-
+        doPost(request, response);
     }
 }
--- mall-web/src/main/webapp/BecomeSeller.jsp
+++ mall-web/src/main/webapp/BecomeSeller.jsp
@@ -9,22 +9,30 @@
 <html>
 <head>
     <title>成为卖家</title>
-  <style>
-    body{
-      background-color: coral;
-      line-height: inherit;
-      text-align: center;
-    }
-    h1{
-      font-style: normal;
-
-    }
-  </style>
+ <style>
+   body{
+     background-color: burlywood;
+   }
+     h2{
+         font-size: 14px;
+         float:left;
+         position: absolute;
+         bottom: 0px;
+         left: 0px;
+         text-align: center;
+         margin-bottom: inherit;
+     }
+ </style>
 </head>
 <body>
 <h1><font color="red">修改成功</font></h1>
 <br>
 <button type="button">返回</button>
 <form action="index.jsp" method="post"></form>
+<iframe name="sell" width="100%" height="200" frameborder="5"></iframe>
+<h2>
+    <ul>联系我们</ul>
+    <ul>站内公告</ul>
+</h2>
 </body>
 </html>
