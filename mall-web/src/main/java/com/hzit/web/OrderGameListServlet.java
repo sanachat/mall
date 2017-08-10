6e285bce76a8f7eac090b8ac7279ef655f9eee63
xiaowan1 <1335422698@QQ.com>
2017-08-10 17:17:42 +0800

    1

---
+++ mall-web/src/main/java/com/hzit/web/OrderGameListServlet.java
@@ -0,0 +1,36 @@
+package com.hzit.web;
+
+import com.hzit.dao.OrderDao;
+import com.hzit.dao.SqlSessionHelper;
+import com.hzit.entity.Order;
+import org.apache.ibatis.session.SqlSession;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by wan on 2017/8/10.
+ */
+@WebServlet(name = "OrderGameListServlet",value = "/OrderGameList")
+public class OrderGameListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html");
+        SqlSession session=SqlSessionHelper.getSqlSession();
+        OrderDao dao= session.getMapper(OrderDao.class);
+        String id=request.getParameter("orderId");
+        List<Order> l=dao.selectgame(Integer.parseInt(id));
+        request.setAttribute("l",l);
+        request.getRequestDispatcher("OrderList.jsp").forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
---
+++ mall-web/src/main/java/com/hzit/web/OrderInsertServlet.java
@@ -0,0 +1,56 @@
+package com.hzit.web;
+
+import com.hzit.dao.OrderDao;
+import com.hzit.dao.SqlSessionHelper;
+import com.hzit.entity.Order;
+import org.apache.ibatis.session.SqlSession;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.DoubleSummaryStatistics;
+
+/**
+ * Created by wan on 2017/8/10.
+ */
+@WebServlet(name = "OrderDeleteServlet",value = "/Orderinsert")
+public class OrderInsertServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html");
+        SqlSession session=SqlSessionHelper.getSqlSession();
+        OrderDao dao= session.getMapper(OrderDao.class);
+        String Id=request.getParameter("orderId");
+        String userId=request.getParameter("userId");
+        String date=request.getParameter("date");
+        String price=request.getParameter("price");
+        Order o=new Order();
+        o.setOrderId(Integer.parseInt(Id));
+        o.setUserId(Integer.parseInt(userId));
+        o.setDate(date);
+        o.setPrice(Double.parseDouble(price));
+        int num=dao.insert(o);
+        session.commit();
+        PrintWriter ou = response.getWriter();
+        if(num==1){
+            System.out.println(num);
+            ou.print("添加订单成功");
+
+        }
+        else{
+            session.rollback();
+            System.out.println("增加失败");
+            ou.print("添加订单失败");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+
+    }
+}
