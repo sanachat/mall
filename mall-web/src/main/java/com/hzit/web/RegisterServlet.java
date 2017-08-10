4051ad466d65820765b643dfa8e162ec623a6727
pengmeiqian <1172648900@qq.com>
2017-08-10 18:11:02 +0800

    3

--- mall-web/src/main/java/com/hzit/web/RegisterServlet.java
+++ mall-web/src/main/java/com/hzit/web/RegisterServlet.java
@@ -1,5 +1,10 @@
 package com.hzit.web;
 
+import com.hzit.dao.SqlSessionHelper;
+import com.hzit.dao.UserInfoDao;
+import com.hzit.entity.UserInfo;
+import org.apache.ibatis.session.SqlSession;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +18,21 @@ import java.io.IOException;
 @WebServlet(name = "RegisterServlet")
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        SqlSession session= SqlSessionHelper.getSqlSession();
+        UserInfoDao dao=session.getMapper(UserInfoDao.class);
+        String name=request.getParameter("loginName");
+        String pwd=request.getParameter("loginPwd");
+        UserInfo user=new UserInfo();
+        user.setLoginName(name);
+        user.setLoginPwd(pwd);
+        UserInfo userInfo=dao.checkLogin(user);
+        if(userInfo==null){
+            response.sendRedirect("register.html");
+        }else{
+            request.getSession().setAttribute("user",userInfo);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
