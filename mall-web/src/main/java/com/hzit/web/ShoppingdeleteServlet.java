ec0fa72469a4acf762eeeb2e755325c159930ad2
dengzhiyuyu <2773299016@qq.com>
2017-08-12 14:37:10 +0800

    d

--- mall-web/src/main/java/com/hzit/web/ShoppingdeleteServlet.java
+++ mall-web/src/main/java/com/hzit/web/ShoppingdeleteServlet.java
@@ -1,5 +1,9 @@
 package com.hzit.web;
 
+import com.hzit.dao.ShoppingCartDao;
+import com.hzit.dao.SqlSessionHelper;
+import org.apache.ibatis.session.SqlSession;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +14,27 @@ import java.io.IOException;
 /**
  * Created by THINK on 2017/8/12.
  */
-@WebServlet(name = "ShoppingdeleteServlet")
+@WebServlet(name = "ShoppingdeleteServlet",urlPatterns = "/shoppingdelete.action" )
 public class ShoppingdeleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        request.setCharacterEncoding("utf-8");
+        SqlSession session=SqlSessionHelper.getSqlSession();
+        ShoppingCartDao dao= session.getMapper(ShoppingCartDao.class);
+        int Id=Integer.parseInt(request.getParameter("pid"));
+        int num=0;
+        num=dao.deleteshoppingcart(Id);
+        if (num==1){
+            response.sendRedirect("findAllcart");
+            session.commit();
+        }else {
+            response.getWriter().append("删除失败！");
+            session.rollback();
+        }
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
