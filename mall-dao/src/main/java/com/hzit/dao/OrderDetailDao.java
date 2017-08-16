13fe024ed884d74a3553927661a5ce716b5e9f46
xiaowan1 <1335422698@QQ.com>
2017-08-16 11:52:29 +0800

    xiaow

--- mall-dao/src/main/java/com/hzit/dao/OrderDetailDao.java
+++ mall-dao/src/main/java/com/hzit/dao/OrderDetailDao.java
@@ -2,9 +2,15 @@ package com.hzit.dao;
 
 import com.hzit.entity.OrderDetail;
 
+import java.util.List;
+
 /**
  * Created by wan on 2017/8/14.
  */
 public interface OrderDetailDao {
-    public int insert(OrderDetail orderDetail);
+    public int insert(OrderDetail o);
+    public int update(OrderDetail o);
+    public int delete(OrderDetail o);
+    public List<OrderDetail> findbyid(int orderId);
+    public List<OrderDetail> findall();
 }
